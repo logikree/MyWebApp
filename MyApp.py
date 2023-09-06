a80c425af5e8cddf09532e9f09a3b40eea298f2a
logikree <makeevmax99@gmail.com>
2023-09-06 18:08:12 +0300

    third

--- MyApp.py
+++ MyApp.py
@@ -1,14 +1,14 @@
 def f(a,b,c):
     d=(b**2)-(4*(a*c))
     if d<0:
-        return
+        return 'корней нет'
     if d==0:
         return (-b)/(2*a)
     if d>0:
         x1=(-b+(d**0.5))/(2*a)
         x2=(-b-(d**0.5))/(2*a)
         return x1,x2
-a=int(input())
-b=int(input())
-c=int(input())
+a=float(input())
+b=float(input())
+c=float(input())
 print(f(a,b,c))
